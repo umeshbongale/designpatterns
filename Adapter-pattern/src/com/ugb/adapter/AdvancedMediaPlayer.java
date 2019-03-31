985c1efb31885ccb71617e3722c9b3484d85d80a
unknown <umeshbongale@gmail.com>
2019-03-31 22:22:42 +0530

    Adapter design pattern implementation

---
+++ Adapter-pattern/src/com/ugb/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.ugb.adapter;
+
+public interface AdvancedMediaPlayer {	
+	   public void playVlc(String fileName);
+	   public void playMp4(String fileName);
+	}
---
+++ Adapter-pattern/src/com/ugb/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.ugb.adapter;
+
+public interface MediaPlayer {
+	   public void play(String audioType, String fileName);
+}
